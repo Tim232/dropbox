8b5ed9e06975d7f5643f26a012cf0f7ceb738360
Tim232 <junseongi284@gmail.com>
2021-01-28 22:57:32 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,13 +5,12 @@ class Dropbox:
     def __init__(self, token):
         self.token = token
         print(self.token)
-
-    def files_upload(self, file:bytes, name:str):
         if not os.path.isdir('dropboxFiles'):
             os.mkdir('dropboxFiles')
-        now = 'dropboxFiles/'+str(datetime.datetime.now().strftime("%Y%m%d%H"))
+
+    def files_upload(self, file:bytes, name:str):
+        now = f'dropboxFiles/{datetime.datetime.now().strftime("%Y%m%d%H")}'
         if not os.path.isdir(now):
             os.mkdir(now)
-        name = now+"/"+name
-        with open(name, 'wb') as f:
+        with open(f"{now}/{name}", 'wb') as f:
             f.write(file)
